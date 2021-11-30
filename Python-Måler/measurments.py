9ab5d0c45124c9a36b5b93370ca98ee6e392b077
niko7381 <42538560+niko7381@users.noreply.github.com>
2021-11-30 14:56:29 +0100

    Hul igennem til serven og re structuret

--- Python-Måler/measurments.py
+++ Python-Måler/measurments.py
@@ -1,28 +1,23 @@
 # imports
 from sense_hat import SenseHat
-# import requests
+import requests
 import time
 from uuid import getnode as get_mac
 
 
 # Looping
 while True:
-    mac = get_mac()
-    print("Mac Adress:", hex(mac))
-    
-    print("Sleeping in 5 seconds")
-    time.sleep(5)
-
+    # getting pressure, temp, humidity and mac from sensehat
     sense = SenseHat()
     sense.clear()
 
-    # getting pressure, temp and humidity from sensehat
     try:
         pressure = sense.get_pressure()
         temp = sense.get_temperature()
         humidity = sense.get_humidity()
-    except NameError:
-        print("Naming error")
+        mac = get_mac()
+    except NameError as err:
+        print(err)
     except:
         print("Something went wrong")
     
@@ -30,19 +25,31 @@ while True:
     print("Pressure is: %.6s Millibars" % pressure)
     print("Temperature is: %.5s C" % temp)
     print("Humidity is: %.5s %%rH \n" % humidity)
+    print("Mac Adress:", hex(mac))
 
 
     # Sending data to the server
-    # requests.post('http://127.0.0.1/save_data', data = { 'pressure': pressure, 'temp': temp, 'humidity': humidity })
+    response = requests.post('http://10.42.0.1:3000/data/save_data', data = { 
+        'pressure': pressure, 
+        'temp': temp, 
+        'humidity': humidity, 
+        'macaddress': mac 
+    })
+
+    # a little HTTP Error handling
+    if (response.status_code == 200):
+        print("The request was a success!")
+        # Code here will only run if the request is successful
+
+    elif (response.status_code == 404):
+        print("Result not found!")
+        # Code here will react to failed requests
+    
+    # print out http response
+    print(response.json()["waitTime"])
 
 
-    # # a little HTTP Error handling
-    # if (response.status_code == 200):
-    #     print("The request was a success!")
-    #     # Code here will only run if the request is successful
-    # elif (response.status_code == 404):
-    #     print("Result not found!")
-    #     # Code here will react to failed requests
 
-    # # print out http response
-    # print(response.headers[:]) 
+    # should take sleep number from response of response
+    print("Sleeping in 5 seconds")
+    time.sleep(5)
