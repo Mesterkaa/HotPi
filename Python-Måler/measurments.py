dcb443918ad0f2b38d9eabafb5f7a4f9a5c00b82
niko7381 <42538560+niko7381@users.noreply.github.com>
2021-12-02 13:01:15 +0100

    Function wait, color errors

--- Python-Måler/measurments.py
+++ Python-Måler/measurments.py
@@ -2,7 +2,25 @@
 from sense_hat import SenseHat
 import requests
 import time
+from time import sleep
 from uuid import getnode as get_mac
+from getmac import get_mac_address as gma
+
+waitTime = 5
+
+
+def wait(color, waitTime):
+    i = 0
+    red = color
+    while  i < waitTime * 2: 
+        if i % 2 == 0:
+            sense.clear((red))
+        else:
+            sense.clear()
+        sleep(0.5)
+        i += 1
+        print("i:", i)
+
 
 
 # Looping
@@ -15,7 +33,7 @@ while True:
         pressure = sense.get_pressure()
         temp = sense.get_temperature()
         humidity = sense.get_humidity()
-        mac = get_mac()
+        mac = gma(interface="wlan0")
     except NameError as err:
         print(err)
     except:
@@ -24,32 +42,29 @@ while True:
     # printing out the mesurements
     print("Pressure is: %.6s Millibars" % pressure)
     print("Temperature is: %.5s C" % temp)
-    print("Humidity is: %.5s %%rH \n" % humidity)
-    print("Mac Adress:", hex(mac))
-
+    print("Humidity is: %.5s %%rH" % humidity)
+    print("Mac Adress:", mac, "\n")
 
     # Sending data to the server
     response = requests.post('http://10.42.0.1:3000/data/save_data', data = { 
-        'pressure': pressure, 
-        'temp': temp, 
+        'air_pressure': pressure, 
+        'temperature': temp, 
         'humidity': humidity, 
-        'macaddress': mac 
+        'mac': mac 
     })
 
     # a little HTTP Error handling
     if (response.status_code == 200):
-        print("The request was a success!")
-        # Code here will only run if the request is successful
+        green = 28, 252, 3
+        wait(green, waitTime)
 
-    elif (response.status_code == 404):
-        print("Result not found!")
-        # Code here will react to failed requests
-    
-    # print out http response
-    print(response.json()["waitTime"])
+        waitTime = response.json()["waitTime"]
+        print("WaitTime: ", waitTime)
+        
+        
+    elif (response.status_code != 200):
+        red = 255, 0, 0
+        wait(red, waitTime)
 
-
-
-    # should take sleep number from response of response
-    print("Sleeping in 5 seconds")
-    time.sleep(5)
+    waitTime = response.json()["waitTime"]
+    print("WaitTime: ", waitTime)
