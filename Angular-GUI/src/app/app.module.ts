c3ceac6485099c6b734538cb5c458c86d0cc0857
niko7381 <42538560+niko7381@users.noreply.github.com>
2021-12-02 14:26:37 +0100

    menu

--- Angular-GUI/src/app/app.module.ts
+++ Angular-GUI/src/app/app.module.ts
@@ -1,22 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
+import { LayoutModule } from '@angular/cdk/layout';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatIconModule} from '@angular/material/icon';
-import {MatButtonModule} from '@angular/material/button';
+
 import { HighchartsChartModule } from 'highcharts-angular';
 import { GraphPageComponent } from './graph-page/graph-page.component';
 import { SettingsComponent } from './settings/settings.component';
-import {MatSidenavModule} from '@angular/material/sidenav';
 import { MenuComponent } from './menu/menu.component';
-import { LayoutModule } from '@angular/cdk/layout';
-import { MatListModule } from '@angular/material/list';
 import { UnitsComponent } from './units/units.component';
 import { TypeComponent } from './type/type.component';
+
+
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
 
 @NgModule({
   declarations: [
@@ -26,7 +28,6 @@ import { TypeComponent } from './type/type.component';
     MenuComponent,
     UnitsComponent,
     TypeComponent
-
   ],
   imports: [
     BrowserModule,
