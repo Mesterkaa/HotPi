b8237e89d1df75ef2e717313fcab0b122c1cf1b1
Mesterkaa <32128511+Mesterkaa@users.noreply.github.com>
2021-12-11 15:28:03 +0100

    Add api interface files

---
+++ Angular-GUI/src/app/interfaces/IDevice.ts
@@ -0,0 +1,6 @@
+export interface IDevice {
+  _id: string
+  __v: number
+  mac: string
+  name: string
+}
---
+++ Angular-GUI/src/app/interfaces/ISetting.ts
@@ -0,0 +1,6 @@
+export interface ISetting {
+  _id: string
+  __v: number
+  name: string
+  value: any
+}
